1b2b6fef5524003d87fed7e427231ceb65faa331
OmarDarwish777 <omar.darwish@blnk.ai>
2024-10-28 15:51:44 +0300

    borrow history model

---
+++ models/BorrowHistory.js
@@ -0,0 +1,58 @@
+// models/User.js
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js'; // adjust path as needed
+import { Book } from './Book.js';
+import { Borrower } from './Borrower.js';
+
+export const BorrowHistoryType = {
+    BORROWED: 1,
+    RETURNED: 2
+};
+
+export const BorrowHistory = sequelize.define('BorrowHistory',{
+    borrowerId:{
+        type:DataTypes.INTEGER,
+        references:{
+            model:Borrower,
+            key:'id'
+        }
+    },
+    bookId:{
+        type:DataTypes.INTEGER,
+        references:{
+            model:Borrower,
+            key:'id'
+        }
+    },
+    status:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: BorrowHistoryType.BORROWED,
+        validate: {
+            isIn: [[BorrowHistoryType.BORROWED, BorrowHistoryType.RETURNED]]
+        }
+    },
+    borrowedDate:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    returnedDate:{
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    dueDate:{
+        type:DataTypes.DATE,
+        allowNull:false
+    }
+
+},{ freezeTableName: true });
+
+BorrowHistory.belongsTo(Borrower, {
+    foreignKey: 'borrowerId',
+    onDelete: 'CASCADE'
+});
+
+BorrowHistory.belongsTo(Book, {
+    foreignKey: 'bookId',
+    onDelete: 'CASCADE'
+});
