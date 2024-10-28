583f32c92ef15a27e91e0c38580f6be7f7873ebf
OmarDarwish777 <omar.darwish@blnk.ai>
2024-10-28 15:50:33 +0300

    added routes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 import express from 'express';
 import BookRoutes from './routes/BookRoutes.js'
+import BorrowerRoutes from './routes/BorrowerRoutes.js'
+import BorrowingRoutes from './routes/BorrowingRoutes.js'
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/books', BookRoutes);
+app.use('/borrowers',BorrowerRoutes);
+app.use('/borrowing',BorrowingRoutes);
 
 
 
